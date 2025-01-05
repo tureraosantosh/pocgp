6e29474bc3472e2a3b4c4008991edc240ce1c63f
tureraosantosh <tureraosantosh@gmail.com>
2025-01-05 21:33:22 +0530

    updated

---
+++ src/test/java/com/example/pocgp/PocgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
